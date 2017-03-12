9ddf35ddec1c119ec58d79ed77761da5f6f63eec
Stephen Kelvin Justo <stephenjusto24@gmail.com>
2017-03-11 18:55:14 -0800

    3/11/2017 6:53 PM
    I added a player. I created a PixelPerfectCamera script which modifies
    the camera. And lastly I made a PlayerMovement script.

---
+++ King of America/Assets/Scripts/PixelPerfectCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelPerfectCamera : MonoBehaviour {
+
+	void Awake()
+	{
+		var camera = GetComponent<Camera> ();
+		if (camera.orthographic) {
+			camera.orthographicSize = (Screen.height / 2f) / 100f;
+		}
+	}
+}
---
+++ King of America/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = .05f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Movement ();
+	}
+
+	void Movement(){
+		Vector3 thisPosition = this.transform.position;
+
+		if (Input.GetKey (KeyCode.W)) {
+			thisPosition.y += speed;
+		}
+		else if (Input.GetKey (KeyCode.A)) {
+			thisPosition.x -= speed;
+		}
+		else if (Input.GetKey (KeyCode.D)) {
+			thisPosition.x += speed;
+		}
+		else if (Input.GetKey (KeyCode.S)) {
+			thisPosition.y -= speed;
+		}
+		this.transform.position = thisPosition;
+	}
+}
