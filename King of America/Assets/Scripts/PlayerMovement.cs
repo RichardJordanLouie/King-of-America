a31262927e69e0621b2a5125cafbf2f0ac58c2ba
Stephen Kelvin Justo <stephenjusto24@gmail.com>
2017-03-12 18:06:53 -0700

    3/12/2017 6:06 PM
    Stephen - added 2 images. changed movement in playermovement script.
    might have done something else.. i forget though.

--- King of America/Assets/Scripts/PlayerMovement.cs
+++ King of America/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed = .035f; //how fast the player moves.
-	public int health = 25; //Player health
+	private int health; //Player health
+	public int maximumHealth = 25;
 	private bool isDead; // is the player dead?
 	private bool facingRight = true; //Is the player facing to the right
+	public GameObject healthBar;
+	public GameObject staminaBar;
+	private Rigidbody2D myBody;
 	Animator anim;
 	void Start()
 	{
+		myBody = GetComponent<Rigidbody2D> ();
+		health = maximumHealth;
 		anim = GetComponent<Animator> ();
 	}
 
 	void Update()
 	{
-		GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+		healthBar.transform.localScale = new Vector3 (health / maximumHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
 		isDead = health <= 0; // if health is less than or equal to zero, then player is dead.
 		if (facingRight == true) {
 			GetComponent<SpriteRenderer> ().flipX = false; // if player is facing to the right, then don't horizontally flip the sprite.
@@ -24,7 +30,6 @@ public class PlayerMovement : MonoBehaviour {
 		else if (facingRight == false) {
 			GetComponent<SpriteRenderer> ().flipX = true; // if player is not facing to the right, then horizontally flip the sprite.
 		}
-
 
 	}
 
@@ -35,51 +40,54 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	void Movement(){
-		Vector3 thisPosition = this.transform.position;
 		if (Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.D)) {
-			thisPosition.y += (speed / (Mathf.Sqrt(2f))) * Time.deltaTime;
-			thisPosition.x += (speed / (Mathf.Sqrt(2f))) * Time.deltaTime;
+			myBody.velocity = new Vector2 ((speed / (Mathf.Sqrt(2f))) * Time.deltaTime,(speed / (Mathf.Sqrt(2f))) * Time.deltaTime);
 			facingRight = true;
 			anim.SetTrigger ("Right");
+			return;
 		}
 		else if (Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.A)) {
-			thisPosition.y += (speed / (Mathf.Sqrt(2f))) * Time.deltaTime;
-			thisPosition.x -= (speed / (Mathf.Sqrt(2f))) * Time.deltaTime;
+			myBody.velocity = new Vector2 ((-speed / (Mathf.Sqrt(2f))) * Time.deltaTime,(speed / (Mathf.Sqrt(2f))) * Time.deltaTime);
 			facingRight = false;
 			anim.SetTrigger ("Right");
+			return;
 		}
 		else if (Input.GetKey (KeyCode.S) && Input.GetKey (KeyCode.D)) {
-			thisPosition.y -= (speed / (Mathf.Sqrt(2f))) * Time.deltaTime;
-			thisPosition.x += (speed / (Mathf.Sqrt(2f))) * Time.deltaTime;
+			myBody.velocity = new Vector2 ((speed / (Mathf.Sqrt(2f))) * Time.deltaTime,(-speed / (Mathf.Sqrt(2f))) * Time.deltaTime);
 			facingRight = true;
 			anim.SetTrigger ("Right");
+			return;
 		}
 		else if (Input.GetKey (KeyCode.S) && Input.GetKey (KeyCode.A)) {
-			thisPosition.y -= (speed / (Mathf.Sqrt(2f))) * Time.deltaTime;
-			thisPosition.x -= (speed / (Mathf.Sqrt(2f))) * Time.deltaTime;
+			myBody.velocity = new Vector2 ((-speed / (Mathf.Sqrt(2f))) * Time.deltaTime,(-speed / (Mathf.Sqrt(2f))) * Time.deltaTime);
 			facingRight = false;
 			anim.SetTrigger ("Right");
+			return;
 		}
 		else if (Input.GetKey (KeyCode.W)) {
-			thisPosition.y += speed * Time.deltaTime;
+			myBody.velocity = new Vector2 (0f,speed * Time.deltaTime);
 			facingRight = true;
 			anim.SetTrigger ("Up");
+			return;
 		}
 		else if (Input.GetKey (KeyCode.A)) {
-			thisPosition.x -= speed * Time.deltaTime;
+			myBody.velocity = new Vector2 (-speed * Time.deltaTime, 0);
 			facingRight = false;
 			anim.SetTrigger ("Right");
+			return;
 		}
 		else if (Input.GetKey (KeyCode.D)) {
-			thisPosition.x += speed * Time.deltaTime;
+			myBody.velocity = new Vector2 (speed * Time.deltaTime,0);
 			facingRight = true;
 			anim.SetTrigger ("Right");
+			return;
 		}
 		else if (Input.GetKey (KeyCode.S)) {
-			thisPosition.y -= speed * Time.deltaTime;
+			myBody.velocity = new Vector2 (0f,-speed * Time.deltaTime);
 			facingRight = true;
 			anim.SetTrigger ("Down");
+			return;
 		}
-		this.transform.position = thisPosition;
+		myBody.velocity = new Vector2 (0f,0f);
 	}
 }
